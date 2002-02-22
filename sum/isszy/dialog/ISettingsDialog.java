c159e4e4d4c1bea0942cf362c5763d312249358d
skhanna <>
2002-02-22 03:16:27 +0000

    Started work on the settings dialog

---
+++ sum/isszy/dialog/ISettingsDialog.java
@@ -0,0 +1,14 @@
+package sum.isszy.dialog;
+
+import java.awt.Frame;
+import java.awt.
+import javax.swing.JDialog;
+
+
+public class ISettingsDialog extends JDialog
+{
+    public ISettingsDialog()
+    {
+	super(new Frame(),"Isszy Preferences");
+    }
+}

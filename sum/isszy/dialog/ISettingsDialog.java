0dab4e9dd99fc57b740ab438979b9edc7ce35315
skhanna <>
2002-03-04 15:07:43 +0000

    Added stuff to Dialog box

--- sum/isszy/dialog/ISettingsDialog.java
+++ sum/isszy/dialog/ISettingsDialog.java
@@ -3,10 +3,14 @@ package sum.isszy.dialog;
 import java.awt.Frame;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import javax.swing.JDialog;
 import javax.swing.JTabbedPane;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class ISettingsDialog extends JDialog
 {
@@ -18,17 +22,47 @@ public class ISettingsDialog extends JDialog
 
 
     private JTabbedPane tablet = new JTabbedPane();
-
-
+      private JPanel p_general = new JPanel();
+        private JTextField tf_general_idir = new JTextField();
+        private JButton b_general_browse = new JButton("Browse");
+        private JCheckBox cb_general_save = new JCheckBox();
+      private JPanel p_image = new JPanel();
+        private JCheckBox cb_image_zoom = new JCheckBox();
+        private JCheckBox cb_image_center = new JCheckBox();
+        private JCheckBox cb_image_slide = new JCheckBox();
+      private JPanel p_sorting = new JPanel();
+        private JTextField tf_sorting_sdir = new JTextField();
+        private JCheckBox cb_sorting_prompt = new JCheckBox();
     private JPanel p_buttons = new JPanel(new FlowLayout());
       private JButton b_ok = new JButton ("OK");
       private JButton b_cancel = new JButton ("Cancel");
+      private JButton b_apply = new JButton ("Apply");
 
     private void construct()
     {
+	//construct General Tab
+
+	//construct Image tab
+	p_image.setLayout(new GridLayout(3,1));
+	p_image.add(cb_image_center);
+	p_image.add(new JLabel("Center Image"));
+	p_image.add(cb_image_zoom);
+        p_image.add(new JLabel("Zoom Image"));
+	p_image.add(cb_image_slide);
+	p_image.add(new JLabel("Slide Show Multiple Images"));
+
+	//construct Sorting Tab
+
+
+	//put all tabs on table
+	tablet.addTab("General",p_general);
+	tablet.addTab("Image",p_image);
+	tablet.addTab("Sorting",p_sorting);
+
 	//construct buttom OK/Cancel area
 	p_buttons.add(b_ok);
 	p_buttons.add(b_cancel);
+	p_buttons.add(b_apply);
 
 	//put everything together
 	getContentPane().add(tablet, BorderLayout.CENTER);
@@ -39,3 +73,4 @@ public class ISettingsDialog extends JDialog
 	setResizable(false);
     }
 }
+

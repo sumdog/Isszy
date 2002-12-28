74ca36eeaeee7fecc040449d7a326f59a5aaa080
skhanna <>
2002-12-28 13:38:12 +0000

    Modified version to 2.0 This will be released on Freshmeat as Isszy 2.0

--- sum/isszy/Isszy.java
+++ sum/isszy/Isszy.java
@@ -11,7 +11,7 @@ import sum.isszy.dialog.IInitialDialog;
 public class Isszy
 {
 	
-	public static final String VERSION = "1.1 beta";
+	public static final String VERSION = "2.0";
    
     public static void main(String[]args)
     {
